251654f756afd55b9295dccb97f4363f65949b8f
allan888 <admin@crawlingcrab.cn>
2025-10-19 14:51:33 +0800

    增加了打开摄像头的能力

--- WarningUnlock/Form1.Designer.cs
+++ WarningUnlock/Form1.Designer.cs
@@ -30,17 +30,32 @@ partial class Form1
     private void InitializeComponent()
     {
         System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+        button1 = new System.Windows.Forms.Button();
         SuspendLayout();
+        // 
+        // button1
+        // 
+        button1.Font = new System.Drawing.Font("思源黑体 CN Medium", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+        button1.Location = new System.Drawing.Point(267, 173);
+        button1.Name = "button1";
+        button1.Size = new System.Drawing.Size(280, 89);
+        button1.TabIndex = 0;
+        button1.Text = "打开/关上";
+        button1.UseVisualStyleBackColor = true;
+        button1.Click += button1_Click;
         // 
         // Form1
         // 
         AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
         AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         ClientSize = new System.Drawing.Size(800, 450);
+        Controls.Add(button1);
+        Icon = ((System.Drawing.Icon)resources.GetObject("$this.Icon"));
         Text = "CJY安全卫士";
-        Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
         ResumeLayout(false);
     }
+
+    private System.Windows.Forms.Button button1;
 
     #endregion
 }
--- WarningUnlock/Form1.cs
+++ WarningUnlock/Form1.cs
@@ -1,9 +1,39 @@
+using OpenCvSharp;
+
 namespace WarningUnlock;
 
 public partial class Form1 : Form
 {
+    private VideoCapture capture;
+    private Thread cameraThread;
+    private bool isRunning = false;
     public Form1()
     {
         InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        if (!isRunning)
+        {
+            capture = new VideoCapture(0); // 0 表示默认摄像头
+            cameraThread = new Thread(new ThreadStart(CaptureCameraCallback));
+            cameraThread.Start();
+            isRunning = true;
+        }
+        else
+        {
+            isRunning = false;
+            cameraThread?.Join();
+            capture?.Release();
+        }
+    }
+    private void CaptureCameraCallback()
+    {
+        Mat frame = new Mat();
+        while (isRunning)
+        {
+            capture.Read(frame);
+        }
     }
 }
